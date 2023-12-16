f9483baad471f5ee03d749b71007a02ba2c48e6a
hedonicadapter <filtersome@gmail.com>
2023-12-16 17:48:39 +0100

    more or less just lights and scroll animations

--- src/components/carModel.js
+++ src/components/carModel.js
@@ -2,8 +2,13 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-
-import { mediaQuery } from '../util/helpers';
+// import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Lensflare, LensflareElement } from 'three/addons/objects/Lensflare.js';
+import {
+  getRandomInt,
+  mediaQuery,
+  reverseInterpolateClamped,
+} from '../util/helpers';
 
 const isntMobile = mediaQuery('(min-width: 1024px)');
 const sizeMultiplier = isntMobile ? 1 : 0.4;
@@ -11,14 +16,19 @@ const sizeMultiplier = isntMobile ? 1 : 0.4;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   20,
-  window.innerWidth / (window.innerHeight * sizeMultiplier),
+  window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-camera.position.set(-0.8, 1, 9); //x,y,z
+camera.position.set(-0.6, 1, 12); //x,y,z
 camera.rotation.x = -0.1;
+scene.position.y = -1.4;
 
-const renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+const renderer = new THREE.WebGLRenderer({
+  alpha: true,
+  // antialias: true
+});
+renderer.setClearColor(0x000000, 0);
 renderer.setSize(window.innerWidth, window.innerHeight * sizeMultiplier);
 document.getElementById('model-container').appendChild(renderer.domElement);
 
@@ -32,34 +42,55 @@ envLoader.load('/environments/MR_INT-005_WhiteNeons_NAD.hdr', (texture) => {
   scene.environment = texture;
 });
 
+const textureLoader = new THREE.TextureLoader();
+const textureFlare0 = textureLoader.load('lens-flare.png');
+
+function createLensflare(x, y, z) {
+  const lensFlare = new Lensflare();
+
+  lensFlare.position.set(x, y, z);
+  lensFlare.addElement(new LensflareElement(textureFlare0, 700, 0));
+
+  return lensFlare;
+}
+
 const modelLoader = new GLTFLoader();
 
 modelLoader.load(
   'car.glb',
   function (gltf) {
     const modelScene = gltf.scene;
-    modelScene.rotation.y = -0.4;
+    modelScene.rotation.y = -0.6;
 
     for (let i = 0; i < 3; i++) {
       const clonedModel = modelScene.clone();
 
-      clonedModel.position.x = i * -1;
+      // Add lens flares to the scene
+      const leftHeadlightFlare = createLensflare(-0.68, 0.7, 2.265 - i * 0.005);
+      clonedModel.add(leftHeadlightFlare);
+
+      const rightHeadlightFlare = createLensflare(0.67, 0.7, 2.265 - i * 0.005);
+      clonedModel.add(rightHeadlightFlare);
+
       clonedModel.position.z = i * -3;
+      clonedModel.position.x = i * -1;
 
       scene.add(clonedModel);
     }
   },
+
   undefined,
   function (error) {
     console.error(error);
   }
 );
 
-const composer = new EffectComposer(renderer);
+// const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
 
+  // controls.update(); // Update OrbitControls
   renderer.render(scene, camera);
   // composer.render();
 }
@@ -70,10 +101,29 @@ window.addEventListener('resize', () => {
   const isntMobile = mediaQuery('(min-width: 1024px)');
 
   const newWidth = window.innerWidth;
-  const newHeight = isntMobile ? window.innerHeight : window.innerHeight * 0.4;
+  const newHeight = isntMobile
+    ? window.innerHeight
+    : window.innerHeight * sizeMultiplier;
 
   camera.aspect = newWidth / newHeight;
   camera.updateProjectionMatrix();
 
   renderer.setSize(newWidth, newHeight);
 });
+
+window.addEventListener('scroll', () => {
+  const scrollY = window.scrollY;
+  const scrollPercent =
+    (scrollY / (document.body.scrollHeight - window.innerHeight)) * 0.1;
+
+  camera.position.y = 1 - reverseInterpolateClamped(0, 1, scrollPercent * 2);
+  camera.position.x = scrollPercent * (8 - -0.6) + -0.6;
+  camera.position.z = interpolate(12, 20, scrollPercent * 0.3);
+  camera.rotation.y =
+    (reverseInterpolateClamped(0, 1, scrollPercent) * (6 - -0.1) + -0.1) * 0.1;
+
+  scene.position.y = interpolate(-1.4, -4, scrollPercent); // Modify this line
+});
+function interpolate(start, end, progress) {
+  return start + (end - start) * progress;
+}
--- src/util/helpers.js
+++ src/util/helpers.js
@@ -4,3 +4,13 @@ export const mediaQuery = (query) => {
   if (res.matches) return true;
   return false;
 };
+
+export const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const reverseInterpolateClamped = (min, max, value) => {
+  return Math.min(1, Math.max(0, (value - min) / (max - min)));
+};
