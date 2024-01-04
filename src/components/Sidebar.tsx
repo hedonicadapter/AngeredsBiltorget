fe68117a5a52b0924ea26830553bf3dbde94e592
hedonicadapter <filtersome@gmail.com>
2024-01-04 17:32:05 +0100

    sidebar animation

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -7,7 +7,7 @@ export default function Sidebar({ anchors }: { anchors?: any }) {
 
   return (
     <div
-      className={`fixed w-[133vw] h-screen flex flex-row ${
+      className={`fixed w-[133vw] h-screen flex flex-row transition-all ${
         $sidebarOpen
           ? 'z-[2000] right-0 pointer-events-auto'
           : 'z-auto -right-[33vw] pointer-events-none'
@@ -15,14 +15,13 @@ export default function Sidebar({ anchors }: { anchors?: any }) {
     >
       <div
         onClick={() => $sidebarOpen && sidebarOpen.set(false)}
-        className={`w-screen h-screen transition-[backdrop-filter0.5sease-in] pointer-events-auto ${
+        className={`w-screen h-screen pointer-events-auto ${
           $sidebarOpen
             ? 'pointer-events-auto bg-bg-transparent backdrop-blur-md'
             : 'pointer-events-none bg-[#00000000]'
         }`}
       ></div>
       <div
-        // onClick={(evt) => evt.stopPropagation()}
         className={` transition-[right0.25sease-out] top-0 -right-[33vw] bg-surface w-[33vw] h-screen p-4 pt-44 
         ${$sidebarOpen ? ' pointer-events-auto' : ' pointer-events-none'}
         `}
